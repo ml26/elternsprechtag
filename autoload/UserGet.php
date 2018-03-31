e74eda9803a0bf3a298499ef89e8e67e4dbc9219
Michael Lechner <github@ml26.net>
2018-03-31 23:26:09 +0200

    Use the login_code to login

--- autoload/UserGet.php
+++ autoload/UserGet.php
@@ -1,57 +1,55 @@
 <?php
 class UserGet extends HandlerBase
 {
-	function index($f3) {
-		if( $this->isLoggedIn($f3) ) {
-			echo Template::instance()->render('index.htm');
-		} else {
-			$f3->reroute('/login');
-		}
+    function index($f3) {
+        if( $this->isLoggedIn($f3) ) {
+            echo Template::instance()->render('index.htm');
+        } else {
+            $f3->reroute('/login');
+        }
     }
-	
-	function admin($f3) {
-		$f3->reroute('/admin/index');
+    
+    function admin($f3) {
+        $f3->reroute('/admin/index');
     }
-	
-	function login($f3) {
-		if( $this->isLoggedIn($f3) ) {
-			$f3->reroute('/');
-		} else {
-			$allSchueler = DbWrapper::getAllSchueler($f3->get('db'));
-			$f3->set('allSchueler', $allSchueler);
-			echo Template::instance()->render('login.htm');
-		}
+    
+    function login($f3) {
+        if( $this->isLoggedIn($f3) ) {
+            $f3->reroute('/');
+        } else {
+            echo Template::instance()->render('login.htm');
+        }
     }
-	
-	function logout($f3) {
-		if( $this->isLoggedIn($f3) ) {
-			$userId = $f3->get('COOKIE.user_id');
-			Logger::Info($f3, "UserGet.logout", "User: {$userId}");
-			$f3->clear('COOKIE.user_id');
-		}
-		$f3->reroute('/login');
+    
+    function logout($f3) {
+        if( $this->isLoggedIn($f3) ) {
+            $userId = $f3->get('COOKIE.user_id');
+            Logger::Info($f3, "UserGet.logout", "User: {$userId}");
+            $f3->clear('COOKIE.user_id');
+        }
+        $f3->reroute('/login');
     }
-	
-	function impressum($f3) {
-		if( $this->isLoggedIn($f3) ) {
-			$f3->set('location', 'impressum');
-			echo Template::instance()->render('impressum.htm');
-		} else {
-			$f3->reroute('/login');
-		}
+    
+    function impressum($f3) {
+        if( $this->isLoggedIn($f3) ) {
+            $f3->set('location', 'impressum');
+            echo Template::instance()->render('impressum.htm');
+        } else {
+            $f3->reroute('/login');
+        }
     }
-	
-	function reservierungen($f3) {
-		if( $this->isLoggedIn($f3) ) {
-			$f3->set('location', 'user_summary');
-			
-			$db = $f3->get('db');
-			$f3->set('schuelerName', $this->getCurrentSchuelerName($f3));
-			$f3->set('reservations', DbWrapper::getReservationsByUserId($db, $f3->get('COOKIE.user_id')));
-			echo Template::instance()->render('userSummary.htm');
-		} else {
-			$f3->reroute('/login');
-		}
+    
+    function reservierungen($f3) {
+        if( $this->isLoggedIn($f3) ) {
+            $f3->set('location', 'user_summary');
+            
+            $db = $f3->get('db');
+            $f3->set('schuelerName', $this->getCurrentSchuelerName($f3));
+            $f3->set('reservations', DbWrapper::getReservationsByUserId($db, $f3->get('COOKIE.user_id')));
+            echo Template::instance()->render('userSummary.htm');
+        } else {
+            $f3->reroute('/login');
+        }
     }
 }
 
--- autoload/UserPost.php
+++ autoload/UserPost.php
@@ -1,23 +1,22 @@
 <?php
 class UserPost extends HandlerBase
 {
-	function login($f3) {
-		if( $f3->exists('POST.schueler_name') ) {
-			$schuelerName = $f3->get('POST.schueler_name');
-			try {
-				$schuelerId = DbWrapper::getSchuelerIdByName($f3->get('db'), $schuelerName);
-				Logger::Info($f3, "UserPost.login", "Name: {$schuelerName}, Id: {$schuelerId}");
-				$f3->set('COOKIE.user_id', $schuelerId);
-				$f3->set('COOKIE.user_name', $schuelerName);
-				$f3->reroute('/');
-			} catch (Exception $e) {
-				Logger::Error($f3, "UserPost.login", 
-					"Name: {$schuelerName}, Error: {$e->getMessage()}");
-				$f3->reroute('/login');
-			}
-		} else {
-			$f3->reroute('/login');
-		}
+    function login($f3) {
+        if( $f3->exists('POST.login_code') ) {
+            $login_code = $f3->get('POST.login_code');
+            try {
+                $schuelerId = DbWrapper::getSchuelerIdByLoginCode($f3->get('db'), $login_code);
+                Logger::Info($f3, "UserPost.login", "Code: {$login_code}, Id: {$schuelerId}");
+                $f3->set('COOKIE.user_id', $schuelerId);
+                $f3->reroute('/');
+            } catch (Exception $e) {
+                Logger::Error($f3, "UserPost.login", 
+                    "Code: {$login_code}, Error: {$e->getMessage()}");
+                $f3->reroute('/login');
+            }
+        } else {
+            $f3->reroute('/login');
+        }
     }
 }
 
